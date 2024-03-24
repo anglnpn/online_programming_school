1382201d84b8d385235f2d5f5422962fad7caa97
angelina <angln.pnn@gmail.com>
2024-03-24 22:49:23 +0700

    Добавлены serializers
    Signed-off-by: angelina <angln.pnn@gmail.com>

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from materials.models import Course, Lesson, Module
+
+admin.site.register(Course)
+
+admin.site.register(Lesson)
+
+admin.site.register(Module)
+
---
+++ materials/validators.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+class MaterialLinkCustomValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        link_example = 'youtube.com'
+        print(value)
+        tmp_val = dict(value).get(self.field)
+        if link_example not in tmp_val:
+            raise serializers.ValidationError("Разрешены ссылки только на youtube.com")
